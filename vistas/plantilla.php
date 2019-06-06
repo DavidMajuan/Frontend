d9b7eb6bb0eb5e320e38105802bd7cb3855db2ad
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-06 18:53:42 -0500

    agragamos los archivos boostrap.min.css, font-awesome.min.css, jquery.min.js, boostrap.min.js

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <meta name="title" content="ilidanNutrition">
+
+    <meta name="description" content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+    Ut saepe doloribus nisi totam distinctio.">
+
+    <meta name="Keyword" content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+    Ut saepe doloribus nisi totam distinctio.">
+
+    <title>ilidanNutrition</title>
+
+    <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+
+    <script src="js/plugins/jquery.min.js"></script>
+
+    <script src="js/plugins/bootstrap.min.js"></script>
+    
 </head>
 <body>
-    <h1>ilidanNutrition</h1>
+    
+
+
+    
 </body>
 </html>
