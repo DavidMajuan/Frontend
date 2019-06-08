f91e2afb0e881e434a83e941535395bad8dcec32
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-08 12:18:06 -0500

    en el archivo cabexote.php hacemos dinamico el llamadodel logo...usamos la variable  y el llamado del logotipo, con una etiqueta php con un echo, donde ese echo va a traer la variable social con la casilla logo

--- vistas/modulos/cabezote.php
+++ vistas/modulos/cabezote.php
@@ -79,7 +79,7 @@ HEADER
 				
 				<a href="#">
 						
-					<img src="http://localhost:8080/backend/vistas/img/plantilla/logo.png" class="img-responsive">
+					<img src="http://localhost:8080/backend/<?php echo $social["logo"];?>" class="img-responsive">
 
 				</a>
 				 
