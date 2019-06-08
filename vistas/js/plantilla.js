d8bf40817bd5216b7ad8f2dc67425fb43f183fb1
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-08 11:15:35 -0500

    con ayuda de javascript vamos a almcaenar el color de fondo, de texto, barraSuperior, textoSuperior que son las 4 propiedades para modificar el estulo, esas estan en la base de datos

--- vistas/js/plantilla.js
+++ vistas/js/plantilla.js
@@ -8,7 +8,9 @@ $.ajax({
     success:function(respuesta){
 
         var colorFondo = JSON.parse(respuesta).colorFondo;
-
+		var colorTexto = JSON.parse(respuesta).colorTexto;
+		var barraSuperior = JSON.parse(respuesta).barraSuperior;
+		var textoSuperior = JSON.parse(respuesta).textoSuperior;
     }
 
 })
