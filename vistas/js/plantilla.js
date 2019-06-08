f28907f84167c3e070cf1a920e29a28c436bf6e9
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-08 11:10:29 -0500

    luego le digo que me seleccione colorFondo y ya eso lo puedo almacenar en una variable asi: var colorFondo = JSON.parse(respuesta).colorFondo;

--- vistas/js/plantilla.js
+++ vistas/js/plantilla.js
@@ -7,7 +7,7 @@ $.ajax({
     url:"ajax/plantilla.ajax.php",
     success:function(respuesta){
 
-        console.log(JSON.parse(respuesta));
+        var colorFondo = JSON.parse(respuesta).colorFondo;
 
     }
 
