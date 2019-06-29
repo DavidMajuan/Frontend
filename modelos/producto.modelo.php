2c5190321d68aa5d1a50fd3701ed892e5d93c7c6
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-29 02:23:27 -0500

    creamos el metodo estatico, es estatico para poder pasasrle parametros, se le pasa los parametros de la  y el ; hacemos la consulta y con bindParam enlazamos parametros

--- modelos/producto.modelo.php
+++ modelos/producto.modelo.php
@@ -4,10 +4,36 @@ require_once "conexion.php";
 
 class ModeloProductos{
 
+    /*=================================================================
+    MOSTRAR CATEGORIAS
+    =================================================================*/
 
     static public function mdlMostrarCategorias($tabla){
 
         $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+
+        $stmt -> execute();
+
+        return $stmt -> fetchAll();
+
+        $stmt -> close();
+
+        $stmt =null;
+
+
+
+    }
+
+    /*=================================================================
+    MOSTRAR SUBCATEGORIAS
+    =================================================================*/
+
+
+    static public function mdlMostrarSubCategorias($tabla, $id){
+
+        $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE id_categoria = :id_categoria");
+
+        $stmt -> bindParam(":id_categoria", $id, PDO::PARAM_INT);
 
         $stmt -> execute();
 
