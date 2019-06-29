a49658c42172f123847ecc816b16cc87c6098c9f
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-29 02:21:18 -0500

    cremos el metodo ctrMostrarSubactegorias y le pasamos el parametro id

--- controladores/producto.controlador.php
+++ controladores/producto.controlador.php
@@ -2,11 +2,33 @@
 
 class ControladorProductos{
 
+    /*=================================================================
+    MOSTRAR CATEGORIAS
+    =================================================================*/
+
     public function ctrMostrarCategorias(){
 
         $tabla = "categorias";
 
-        $respuesta = ModeloProducto::mdlMostrarCtaegorias($tabla);
+        $respuesta = ModeloProductos::mdlMostrarCategorias($tabla);
+
+        return $respuesta;
+
+
+
+    }
+
+
+
+    /*=================================================================
+    MOSTRAR SUBCATEGORIAS
+    =================================================================*/
+
+    public function ctrMostrarSubcategorias($id){
+
+        $tabla = "subcategorias";
+
+        $respuesta = ModeloProductos::mdlMostrarSubCategorias($tabla, $id);
 
         return $respuesta;
 
