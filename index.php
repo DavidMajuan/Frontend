7759bd0574cbc87df83c0e3b0ba3834605525d4a
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-29 01:33:25 -0500

    agregamos rutas de archivos creados en el modelo y en el controlador

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require_once "controladores/plantilla.controlador.php";
+require_once "controladores/producto.controlador.php";
 require_once "modelos/plantilla.modelo.php";
+require_once "modelos/producto.modelo.php";
 
 require_once "modelos/rutas.php";
 
