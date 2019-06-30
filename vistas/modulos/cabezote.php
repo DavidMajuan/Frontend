f02d6c4d5d7ce60467052ac97df301c70b7ba3d1
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-30 11:08:11 -0500

    se hizo dinamica la ruta del servidor

--- vistas/modulos/cabezote.php
+++ vistas/modulos/cabezote.php
@@ -1,3 +1,9 @@
+<?php
+
+$servidor= Ruta::ctrRutaServidor();
+
+?>
+
 <!--=====================================
 TOP
 ======================================-->
@@ -80,7 +86,7 @@ HEADER
 				
 				<a href="#">
 						
-					<img src="http://localhost:8080/backend/<?php echo $social["logo"];?>" class="img-responsive">
+					<img src="<?php echo $servidor.$social["logo"];?>" class="img-responsive">
 
 				</a>
 				 
