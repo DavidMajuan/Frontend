1ced9b19b0e3bd97b0eb4d327e944019ea3a3b2c
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-30 10:59:46 -0500

    se llama al metodo ctrMostrarSlide de la clase ControladorSlide, hacemos un foreach por que estamos llamando a toda la tabla es decir se ha hecho fetchAll y despues concatenamos campos que seran dinamicos

--- vistas/modulos/slide.php
+++ vistas/modulos/slide.php
@@ -12,145 +12,53 @@ SLIDESHOW
 
 		<ul>
 
-        <!--slide 1-->
+		<?php
 
-			<li>
+		$slide = ControladorSlide::ctrMostrarSlide();
+
+		foreach ($slide as $key => $value) {
+
+		$estiloImgProducto = json_decode($value["estiloImgProducto"],true);
+		$estiloTextoSlide = json_decode($value["estiloTextoSlide"],true);
+		$titulo1 = json_decode($value["Titulo1"],true);
+		$titulo2 = json_decode($value["Titulo2"],true);
+		$titulo3 = json_decode($value["Titulo3"],true);
+			
+		
+		
+
+		echo '<li>
 				
-							<img src="http://localhost:8080/backend/vistas/img/slide/default/back_default.jpg">
+					<img src="http://localhost:8080/backend/'.$value["imgFondo"].'">
 
-							<div class="slideOpciones slideOpcion1">
+					<div class="slideOpciones '.$value["tipoSlide"].'">
+						
+						<img class="imgProducto" src="http://localhost:8080/backend/'.$value["imgProducto"].'" style="top:'.$estiloImgProducto["top"].'; right:'.$estiloImgProducto["right"].'; width:'.$estiloImgProducto["width"].'; left:'.$estiloImgProducto["left"].'">
+
+						<div class="textosSlide" style="top:'.$estiloTextoSlide["top"].'; right:'.$estiloTextoSlide["right"].'; width:'.$estiloTextoSlide["width"].'; left:'.$estiloTextoSlide["left"].'">
+							
+							<h1 style="color:'.$titulo1["color"].'">'.$titulo1["texto"].'</h1>
+
+							<h2 style="color:'.$titulo2["color"].'">'.$titulo2["texto"].'</h2>
+
+							<h3 style="color:'.$titulo3["color"].'">'.$titulo3["texto"].'</h3>
+
+							<a href="'.$value["url"].'">
 								
-								<img class="imgProducto" src="http://localhost:8080/backend/vistas/img/slide/slide1/nutricion.png" style="top:15%; right:10%; width:45%">
-
-								<div class="textosSlide" style="top:20%; left:10%; width:40%">
-									
-									<h1 style="color:#333">Alimentación </h1>
-
-									<h2 style="color:#777">Nutricionistas especializados</h2>
-
-									<h3 style="color:#888">Inscribete y obten descuentos</h3>
-
-									<a href="#">
-										
-										<button class="btn btn-default backColor text-uppercase">
-										
-										VER NUTRICIONISTA <span class="fa fa-chevron-right "></span>
-										
-										</button>
-									
-									</a>
-
-								</div>	
-
-							</div>
-
-			</li>
-
-			<!--SLIDE 2-->
-
-			<li>
-				
-				<img src="http://localhost:8080/backend/vistas/img/slide/default/back_default.jpg">
-
-				<div class="slideOpciones slideOpcion2">
-					
-					<img class="imgProducto" src="http://localhost:8080/backend/vistas/img/slide/slide2/nutricionista.png" style="top:5%; left:15%; width:25%">
-
-					<div class="textosSlide" style="top:20%; right:10%; width:40%">
-						
-						<h1 style="color:#333">Maria Gutierrez </h1>
-
-						<h2 style="color:#777">Profesional capacitada</h2>
-
-						<h3 style="color:#888">Obten asesoria especializada</h3>
-
-						<a href="#">
+							'.$value["boton"].'
 							
-							<button class="btn btn-default backColor text-uppercase">
-							
-							VER NUTRICIONISTA <span class="fa fa-chevron-right "></span>
-							
-							</button>
-						
-						</a>
+							</a>
 
-					</div>	
+						</div>	
 
-				</div>
+					</div>
 
-			</li>
+			</li>';
 
+		}
+		?>
 
-			<!--slide 3-->
-
-			<li>
-				
-				<img src="http://localhost:8080/backend/vistas/img/slide/default/back_default.jpg">
-
-				<div class="slideOpciones slideOpcion2">
-					
-					<img class="imgProducto" src="http://localhost:8080/backend/vistas/img/slide/slide3/nutricionista.png" style="top:5%; left:15%; width:32%">
-
-					<div class="textosSlide" style="top:20%; right:10%; width:40%">
-						
-						<h1 style="color:#333">Francisco Maldonado </h1>
-
-						<h2 style="color:#ccc">Obten su asesoria</h2>
-
-						<h3 style="color:#aaa">Nutricionista especializado</h3>
-
-						<a href="#">
-							
-							<button class="btn btn-default backColor text-uppercase">
-							
-							VER NUTRICIONISTA<span class="fa fa-chevron-right "></span>
-							
-							</button>
-						
-						</a>
-
-					</div>	
-
-				</div>
-
-			</li>
-
-			<!--slide 4-->
-
-			<li>
-				
-				<img src="http://localhost:8080/backend/vistas/img/slide/slide4/fondo3.jpg">
-
-				<div class="slideOpciones slideOpcion1" >
-					
-					<img class="imgProducto" src="" style="top:5%; right:15%; with:25%">
-
-					<div class="textosSlide" style="top:20%; left:10%; width:40%">
-						
-					<h1 style="color:#333">Sofía Montalvo </h1>
-
-					<h2 style="color:#777">Obten Asesoría</h2>
-
-					<h3 style="color:#888">Nutricionista con años de experiencia</h3>
-
-						<a href="#">
-							
-							<button class="btn btn-default backColor text-uppercase">
-							
-							VER NUTRICIONISTA<span class="fa fa-chevron-right "></span>
-							
-							</button>
-
-				
-						</a>
-
-					</div>	
-
-				</div>
-
-			</li>
-
-	
+        
 
 		</ul>
 
@@ -160,12 +68,16 @@ SLIDESHOW
 
 		<ol id="paginacion">
 
+			<?php
 			
+				for ($i=1; $i <= count($slide) ; $i++) { 
+					
+					echo '<li item="'.$i.'"><span class="fa fa-circle"></span></li>';
 
-					<li item="1"><span class="fa fa-circle"></span></li>
-					<li item="2"><span class="fa fa-circle"></span></li>
-					<li item="3"><span class="fa fa-circle"></span></li>
-					<li item="4"><span class="fa fa-circle"></span></li>
+				}
+			
+			?>
+
 
 						
 
