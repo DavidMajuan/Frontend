d2b90c5549a6a374962627dc2dc069bad31d3725
DavidMajuan <davidmajuanpintado@gmail.com>
2019-06-30 10:51:38 -0500

    se crea la clase ControladorSlide y el metodo ctrMostrarSlide, LLamamos a latabla de la base de datos y lo retornamos a la vista

---
+++ controladores/slide.controlador.php
@@ -0,0 +1,21 @@
+<?php
+
+class ControladorSlide{
+
+    public function ctrMostrarSlide(){
+
+        $tabla ="slide";
+
+        $respuesta = ModeloSlide::mdlMostrarSlide($tabla);
+
+        return $respuesta;
+
+
+
+    }
+
+
+
+
+}
+
